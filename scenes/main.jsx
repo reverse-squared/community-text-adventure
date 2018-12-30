fd189325794336d983102f2fb91fbc6c0a01e866
Hunter Parcells <hparcells@icloud.com>
2018-12-30 16:29:29 -0500

    Not paying the bill.

--- scenes/main.jsx
+++ scenes/main.jsx
@@ -8,7 +8,7 @@ export default {
     start: {
         prompt: () => <div>
             <p>You wake up in a room. <b>What do you do?</b></p>
-            <p class="credit">Designed by: dave.</p>
+            <p class="credit">Contributed by: dave.</p>
         </div>,
         options: [
             { text: "Go outstide", to: "outside" },
@@ -21,7 +21,7 @@ export default {
     outside: {
         prompt: () => <div>
             <p>You walk outside from sleeping. You spot multiple different animals such as lizards, spiders, and humans. <b>What do you do?</b></p>
-            <p class="credit">Designed by: Hunter Parcells.</p>
+            <p class="credit">Contributed by: Hunter Parcells.</p>
         </div>,
         options: [
             { text: "Touch the lizard.", to: "lizardTouch" },
@@ -33,8 +33,8 @@ export default {
     // Eat breakfast. Level 2.
     breakfast: {
         prompt: () => <div>
-            <p>You walk downstairs to make some breakfast. <b>What do you make?</b></p>
-            <p class="credit">Designed by: Colyderp.</p>
+            <p>You walk downstairs to make some breakfast.What do you make?</p>
+            <p class="credit">Contributed by: Colyderp.</p>
         </div>,
         options: [
             { text: "Pancakes.", to: "pancakes" },
@@ -48,7 +48,7 @@ export default {
     checkTime: {
         prompt: () => <div>
         <p>You look at your clock to check the time and realize...</p>
-        <p class="credit">Designed by: Anonymous.</p>
+        <p class="credit">Contributed by: Anonymous.</p>
     </div>,
     options: [
         { text: "...you are still sleepy and need more rest.", to: "start" },
@@ -61,7 +61,7 @@ export default {
     pancakes: {
         prompt: () => <div>
             <p>You want to make pancakes, but you have a few types to choose from.</p>
-            <p class="credit">Designed by: dave.</p>
+            <p class="credit">Contributed by: dave.</p>
         </div>,
         options: [
             { text: "Regular.", to: "regularPancakes" },
@@ -71,14 +71,38 @@ export default {
         ]
     },
 
+    // Peanut butter pancakes. Level 4.
     pbPancakes: {
         prompt: () => <div>
             <p>Turns out you are allergic to peanut butter. Now you have to play a hospital and ambulance bill of <b>$4313</b>. Do you pay it?</p>
-            <p class="credit">Designed by: Hunter Parcells.</p>
+            <p class="credit">Contributed by: Hunter Parcells.</p>
         </div>,
         options: [
             { text: "Yes (-$4313)", to: "yesPayBill" },
             { text: "No", to: "noPayBill" }
+        ]
+    },  
+
+    // Don't pay hospital bill. Level 5.
+    noPayBill: {
+        prompt: () => <div>
+            <p>You decide not to pay the bill and...</p>
+            <p class="credit">Contributed by: <a href="https://filipkin.com/">Filip96</a>.</p>
+        </div>,
+        options: [
+            { text: "Run for it.", to: "" },
+            { text: "Take out a loan.", to: "" },
+            { text: "Jump out a window.", to: "" },
+        ]
+    },
+
+    dead: {
+        prompt: () => <div>
+            <p>Uh oh how unfortunate. <b>You died.</b></p>
+            <p class="credit">Contributed by: Hunter Parcells.</p>
+        </div>,
+        options: [
+            { text: "Play again.", to: "start" }
         ]
     }
 }
