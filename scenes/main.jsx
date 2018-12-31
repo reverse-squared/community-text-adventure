b96cca01a69d7d602139ef922bf25db636a097fb
Dave <imdaveead@gmail.com>
2018-12-30 20:33:46 -0500

    contributor is now part of custom html

--- scenes/main.jsx
+++ scenes/main.jsx
@@ -1,54 +1,53 @@
 import React from 'react';
 import { addFlag, addScenes } from 'web-text-adventure';
 
-addFlag("money", 100);
-
 addScenes({
     // Start. Level 1.
     start: {
         prompt: () => <div>
             <p>You wake up in a room. <b>What do you do?</b></p>
-            <p class="credit">Contributed by: dave.</p>
         </div>,
+        
         options: [
             { text: "Go outstide", to: "outside" },
             { text: "Make some breakfast.", to: "breakfast" },
             { text: "Check the time", to: "checkTime" }
-        ]
+        ],
+        
+        contributor: "dave"
     },
 
     // Go outside. Level 2.
     outside: {
         prompt: () => <div>
             <p>You walk outside from sleeping. You spot multiple different animals such as lizards, spiders, and humans. <b>What do you do?</b></p>
-            <p class="credit">Contributed by: Hunter Parcells.</p>
         </div>,
         options: [
             { text: "Touch the lizard.", to: "lizardTouch" },
             { text: "Touch the spider.", to: "spiderTouch" },
             { text: "Touch the human.", to: "humanTouch" }
-        ]
+        ],
+        contributor: "Hunter Parcells"
     },
 
     // Eat breakfast. Level 2.
     breakfast: {
         prompt: () => <div>
             <p>You walk downstairs to make some breakfast.What do you make?</p>
-            <p class="credit">Contributed by: Colyderp.</p>
         </div>,
         options: [
             { text: "Pancakes.", to: "pancakes" },
             { text: "Waffles.", to: "" },
             { text: "Hashbrowns.", to: "" },
             { text: "Omlete.", to: "" }
-        ]
+        ],
+        contributor: "Colyderp"
     },
 
     // Check time. Level 2.
     checkTime: {
         prompt: () => <div>
             <p>You look at your clock to check the time and realize...</p>
-            <p class="credit">Contributed by: Anonymous.</p>
         </div>,
         options: [
             { text: "...you are still sleepy and need more rest.", to: "start" },
@@ -61,48 +60,51 @@ addScenes({
     pancakes: {
         prompt: () => <div>
             <p>You want to make pancakes, but you have a few types to choose from.</p>
-            <p class="credit">Contributed by: dave.</p>
         </div>,
         options: [
             { text: "Regular.", to: "regularPancakes" },
             { text: "McDonalds™ brand.", to: "mcdPancakes" },
             { text: "Chocolate.", to: "chocolatePancakes" },
             { text: "Peanut butter.", to: "pbPancakes" }
-        ]
+        ],
+        contributor: "dave"
     },
 
     // Peanut butter pancakes. Level 4.
     pbPancakes: {
         prompt: () => <div>
             <p>Turns out you are allergic to peanut butter. Now you have to play a hospital and ambulance bill of <b>$4313</b>. Do you pay it?</p>
-            <p class="credit">Contributed by: Hunter Parcells.</p>
         </div>,
         options: [
             { text: "Yes (-$4313)", to: "yesPayBill" },
             { text: "No", to: "noPayBill" }
-        ]
+        ],
+        contributor: "Hunter Parcells"
     },
 
     // Don't pay hospital bill. Level 5.
     noPayBill: {
         prompt: () => <div>
             <p>You decide not to pay the bill and...</p>
-            <p class="credit">Contributed by: <a href="https://filipkin.com/">Filip96</a>.</p>
         </div>,
         options: [
             { text: "Run for it.", to: "" },
             { text: "Take out a loan.", to: "" },
             { text: "Jump out a window.", to: "" },
-        ]
+        ],
+
+        contributor: <a href="https://filipkin.com/">Filip96</a>
     },
 
     dead: {
         prompt: () => <div>
             <p>Uh oh how unfortunate. <b>You died.</b></p>
-            <p class="credit">Contributed by: Hunter Parcells.</p>
+            <p class="credit">Contributed by: .</p>
         </div>,
         options: [
             { text: "Play again.", to: "start" }
-        ]
+        ],
+
+        contributor: "Hunter Parcells"
     }
 });
--- src/custom-html.jsx
+++ src/custom-html.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Prompt, Options, setCustomHTML } from 'web-text-adventure';
 import './style.css';
 
-setCustomHTML(() => <div>
+setCustomHTML((scene) => <div>
     <h1>Community Text Adventure</h1>
     <p>All paths and options are made by different people.</p>
     <br></br>
     <Prompt />
     <Options />
+    <br/>
+    <p className="credit">Scene contributed by: {scene.contributor || "Anonymous"}</p>
 </div>);
