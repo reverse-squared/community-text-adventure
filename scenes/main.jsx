8913e826f68e9008ed0ed582c80cd52506fbfa81
Hunter Parcells <hparcells@icloud.com>
2019-01-02 10:52:27 -0500

    Refactored Scene Names

--- scenes/main.jsx
+++ scenes/main.jsx
@@ -13,9 +13,9 @@ addScenes({
         </div>,
         
         options: [
-            { text: "Go outstide", to: "outside" },
-            { text: "Make some breakfast.", to: "breakfast" },
-            { text: "Check the time", to: "checkTime" }
+            { text: "Go outstide", to: "wakeup_outside" },
+            { text: "Make some breakfast.", to: "wakeup_breakfast" },
+            { text: "Check the time", to: "wakeup_check_time" }
         ],
 
         action: () => {
@@ -26,7 +26,7 @@ addScenes({
     },
 
     // Go outside. Level 2.
-    outside: {
+    wakeup_outside: {
         prompt: () => <div>
             <p>You walk outside from sleeping. You spot multiple different animals such as lizards, spiders, and humans. <b>What do you do?</b></p>
         </div>,
@@ -39,27 +39,27 @@ addScenes({
     },
 
     // Eat breakfast. Level 2.
-    breakfast: {
+    wakeup_breakfast: {
         prompt: () => <div>
             <p>You walk downstairs to make some breakfast.What do you make?</p>
         </div>,
         options: [
-            { text: "Pancakes.", to: "pancakes" },
-            { text: "Waffles.", to: "waffles" },
-            { text: "Hashbrowns.", to: "hashbrowns" },
-            { text: "Omlette.", to: "omelette" }
+            { text: "Pancakes.", to: "make_pancakes" },
+            { text: "Waffles.", to: "make_waffles" },
+            { text: "Hashbrowns.", to: "make_hashbrowns" },
+            { text: "Omlette.", to: "make_omelette" }
         ],
         contributor: "Colyderp"
     },
 
     // Check time. Level 2.
-    checkTime: {
+    wakeup_check_time: {
         prompt: () => <div>
             <p>You look at your clock to check the time and realize...</p>
         </div>,
         options: [
             { text: "...you are still sleepy and need more rest.", to: "sleep" },
-            { text: "...you're hungry and want to eat something.", to: "breakfast" },
+            { text: "...you're hungry and want to eat something.", to: "wakeup_breakfast" },
             { text: "...you have plans to go outside and go on an adventure.", to: "" }
         ],
         // anonymous
@@ -101,68 +101,68 @@ addScenes({
     },
 
     // Pancakes. Level 3.
-    pancakes: {
+    make_pancakes: {
         prompt: () => <div>
             <p>You want to make pancakes, but you have a few types to choose from.</p>
         </div>,
         options: [
-            { text: "Regular.", to: "regularPancakes" },
-            { text: "McDonalds™ brand.", to: "mcdPancakes" },
-            { text: "Chocolate.", to: "chocolatePancakes" },
-            { text: "Peanut butter.", to: "pbPancakes" }
+            { text: "Regular.", to: "make_regular_pancakes" },
+            { text: "McDonalds™ brand.", to: "make_mcd_pancakes" },
+            { text: "Chocolate.", to: "make_chocolate_pancakes" },
+            { text: "Peanut butter.", to: "make_pb_pancakes" }
         ],
         contributor: "Dave"
     },
 
     // Peanut butter pancakes. Level 4.
-    pbPancakes: {
+    make_pb_pancakes: {
         prompt: () => <div>
             <p>Turns out you are allergic to peanut butter. Now you have to play a hospital and ambulance bill of <b>$4313</b>. Do you pay it?</p>
         </div>,
         options: [
-            { text: "Yes (-$4313)", to: "yesPayBill" },
-            { text: "No", to: "noPayBill" }
+            { text: "Yes (-$4313)", to: "yes_pay_bill" },
+            { text: "No", to: "no_pay_bill" }
         ],
         contributor: "Hunter"
     },
 
     // Pay the hospital bill.
-    yesPayBill: {
+    yes_pay_bill: {
         prompt: () => <div>
             <p>You wanna pay your bill, so you have two options, pay with cash and risk getting arrested with counterfeit cash, or pay with one of your 23 credit cards which probably are all maxed out.</p>
         </div>,
         options: [
-            { text: "Pay with Cash", to: "payCash" },
-            { text: "Pay with Credit", to: "payCredit" }
+            { text: "Pay with Cash", to: "pay_bill_cash" },
+            { text: "Pay with Credit", to: "pay_bill_credit" }
         ],
         contributor: "Hunter"
     },
 
-    payCash: {
+    pay_bill_cash: {
         prompt: () => <div>
             <p>You paid with your cash. Even though it was counterfeit, nobody noticed. <s>That won't come back up later.</s> You continue with your day and go outside. What do you do?</p>
         </div>,
         options: [
-            { text: "Fake your death", to: "fakeDeath" },
-            { text: "Go skydiving.", to: "skydive" }
+            { text: "Fake your death", to: "fake_your_death" },
+            { text: "Go skydiving.", to: "go_skydiving" }
         ],
         contributor: "Hunter"
     },
 
     // Don't pay hospital bill. Level 5.
-    noPayBill: {
+    no_pay_bill: {
         prompt: () => <div>
             <p>You decide not to pay the bill and...</p>
         </div>,
         options: [
-            { text: "Run for it.", to: "runFromHospital" },
+            { text: "Run for it.", to: "run_from_hospital" },
             { text: "Take out a loan.", to: "loan_start" },
-            { text: "Jump out a window.", to: "jumpOutOfAWindow" },
+            { text: "Jump out a window.", to: "jump_out_a_window" },
         ],
         contributor: "Filip96"
     },
 
-    jumpOutOfAWindow: {
+    jump_out_a_window: {
         prompt: () => <div>
             You jump out of the window and die, what kind of idea was that supposed to be.
         </div>,
