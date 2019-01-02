218a639f2b1066b3630f288357a2500bea5c38d7
Hunter Parcells <hparcells@icloud.com>
2019-01-02 14:10:37 -0500

    Regular Pancake

--- scenes/main.jsx
+++ scenes/main.jsx
@@ -127,6 +127,18 @@ addScenes({
         contributor: "Dave"
     },
 
+    make_regular_pancakes: {
+        prompt: () => <div>
+            <p>You start making a pancake. What do you do with it.</p>
+        </div>,
+        options: [
+            { text: "Put syrup on it.", to: "regular_pancake_syrup" },
+            { text: "Put butter on it.", to: "regular_pancake_butter" },
+            { text: "Leave it as is.", to: "regular_pancake_leave" },
+            { text: "Rent someone else's pancake.", to: "regular_pancake_rent" }
+        ],
+        contributor: null
+    },
     // Peanut butter pancakes. Level 4.
     make_pb_pancakes: {
         prompt: () => <div>
