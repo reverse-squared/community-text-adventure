5f5c06d2f24c17f03f70a96d6f93b087413874c5
Hunter Parcells <hparcells@icloud.com>
2019-01-04 12:59:10 -0500

    Airplane Crash Ending

--- scenes/adventure.jsx
+++ scenes/adventure.jsx
@@ -14,5 +14,56 @@ addScenes({
             { text: "Walk in the tall grass.", to: "adventure_pokemon_start" }
         ],
         contributor: "Hunter"
-    }
+    },
+
+    // Africa
+    adventure_africa_start: {
+        prompt: () => <div>
+            <p>You board the plane to go to Africa. It is a long flight so you booked first class seating, but United Airlines pulled a No U and downgraded you to a
+                regular seat with no compensation. OwO. The plane takes off normal, and reaches it cruising altitiude. With another ten hours to go, what do you do?
+            </p>
+        </div>,
+        options: [
+            { text: "Sleep", to: "adventure_africa_sleep" },
+            { text: "Stay Woke", to: "adventure_africa_crash_start" },
+            { text: "Jump Out of the Plane", to: "" },
+        ],
+        contributor: "Hunter"
+    },
+
+    adventure_africa_sleep: {
+        prompt: <div>
+            <p>You go to sleep, and realize that you slept so soundly that you never knew the plane crashed on a remote island. Then it burned and exploded due to the leak
+                of engine fuel.
+            </p>
+        </div>,
+        ending: {
+            id: "airplane-crash",
+            name: "Died in a Metal Tube",
+            description: "You slept in too long because you pulled an all nighter at the airport due to a cancelation. Thanks a lot United Airlines."
+        },
+        contributor: "Hunter"
+    },
+
+    // Fortnite
+    adventure_fortnite_start: {
+        prompt: () => <div>
+            <p></p>
+        </div>,
+        options: [
+            { text: "", to: "" },
+        ],
+        contributor: "Hunter"
+    },
+
+    // Pokemon
+    adventure_pokemon_start: {
+        prompt: () => <div>
+            <p></p>
+        </div>,
+        options: [
+            { text: "", to: "" },
+        ],
+        contributor: "Hunter"
+    },
 });
