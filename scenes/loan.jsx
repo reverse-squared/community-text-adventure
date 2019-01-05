d3f85b691338f44b92382713fda3fe9d4d50342f
Hunter Parcells <hparcells@icloud.com>
2019-01-05 17:07:28 -0500

    People in the Hospital

--- scenes/loan.jsx
+++ scenes/loan.jsx
@@ -19,6 +19,10 @@ addFlag("loan_bitcoin", 0);
 addFlag("loan_walletcash", 50);
 addFlag("loan_initial_deposit", 0);
 
+addFlag("askedOldMan", false);
+addFlag("askedLawyer", false);
+addFlag("askedTeen", false);
+
 // amount of Bitcoin you get per one cash
 const BTC_EXCHANGES = [
     5, // loanTurns===30
@@ -524,9 +528,45 @@ addScenes({
             <p>You walk back into the hospital with the possibility that someone will pay off your loan for you. Who do you ask first.</p>
         </div>,
         options: [
-            { text: "The lawyer talking to the receptionist.", to: "" },
-            { text: "The old man in room 842.", to: "" },
-            { text: "The teenager who broke her leg in 629.", to: "" },
+            { text: "The lawyer talking to the receptionist.", to: "loan_lawyer", disabledText: "The lawyer talking to the receptionist.", action: () => askedLawyer = true, if: () => !askedLawyer },
+            { text: "The old man in room 842.", to: "loan_old_man", disabledText: "The old man in room 842.", action: () => askedOldMan = true, if: () => !askedOldMan },
+            { text: "The teenager who broke her leg in 629.", to: "loan_teen", disabledText: "The teenager who broke her leg in 629.", action: () => askedTeen = true, if: () => !askedTeen },
+            { text: "Je🅱us in room ∞.", to: "loan_jebus" },
+        ],
+        contributor: "Hunter"
+    },
+    loan_lawyer: {
+        prompt: () => <div>
+            <p>The lawyer kept talking totally disregarding you.</p>
+        </div>,
+        options: [
+            { text: "The lawyer talking to the receptionist.", to: "loan_lawyer", disabledText: "The lawyer talking to the receptionist.", action: () => askedLawyer = true, if: () => !askedLawyer },
+            { text: "The old man in room 842.", to: "loan_old_man", disabledText: "The old man in room 842.", action: () => askedOldMan = true, if: () => !askedOldMan },
+            { text: "The teenager who broke her leg in 629.", to: "loan_teen", disabledText: "The teenager who broke her leg in 629.", action: () => askedTeen = true, if: () => !askedTeen },
+            { text: "Je🅱us in room ∞.", to: "loan_jebus" },
+        ],
+        contributor: "Hunter"
+    },
+    loan_old_man: {
+        prompt: () => <div>
+            <p>The old man was sleeping so you left him alone.</p>
+        </div>,
+        options: [
+            { text: "The lawyer talking to the receptionist.", to: "loan_lawyer", disabledText: "The lawyer talking to the receptionist.", action: () => askedLawyer = true, if: () => !askedLawyer },
+            { text: "The old man in room 842.", to: "loan_old_man", disabledText: "The old man in room 842.", action: () => askedOldMan = true, if: () => !askedOldMan },
+            { text: "The teenager who broke her leg in 629.", to: "loan_teen", disabledText: "The teenager who broke her leg in 629.", action: () => askedTeen = true, if: () => !askedTeen },
+            { text: "Je🅱us in room ∞.", to: "loan_jebus" },
+        ],
+        contributor: "Hunter"
+    },
+    loan_teen: {
+        prompt: () => <div>
+            <p>"dONt TAlK tO mE UnTIl I HaD MY CoFFeE," she says.</p>
+        </div>,
+        options: [
+            { text: "The lawyer talking to the receptionist.", to: "loan_lawyer", disabledText: "The lawyer talking to the receptionist.", action: () => askedLawyer = true, if: () => !askedLawyer },
+            { text: "The old man in room 842.", to: "loan_old_man", disabledText: "The old man in room 842.", action: () => askedOldMan = true, if: () => !askedOldMan },
+            { text: "The teenager who broke her leg in 629.", to: "loan_teen", disabledText: "The teenager who broke her leg in 629.", action: () => askedTeen = true, if: () => !askedTeen },
             { text: "Je🅱us in room ∞.", to: "loan_jebus" },
         ],
         contributor: "Hunter"
@@ -535,7 +575,7 @@ addScenes({
         prompt: () => <div>
             <p>You enter the elevator to go to the highly talked about Je🅱us. All your friends say that he helped them, so he must help you.</p>
             <p>You arrive in Je🅱us' office and he offers you to pay your loan. He gives you the 
-                <RainbowCircleText string="All Holy, Brand New 2019, One of a Kind, Premium, Limited Edition, Never Seen Before, Deluxe Pro Plus, All Natrual, Grass Fed, No GMO, Vegetarian, Feminist and Flat Earther Free, Version 2019.2a CC for Binbows 9... 🅱" />
+                <RainbowCircleText string="All Holy, Brand New 2019, One of a Kind, Premium, Limited Edition, Never Seen Before, Deluxe Pro Plus, All Natrual, Grass Fed, No GMO, Vegetarian, Feminist and Flat Earther Free, Version 2019.2a CC for Binbows 9... Dank B Emoji" />
             </p>
             <p>Maybe you can get some money for it.</p>
             <p className="inventory-update">
