e93a1e7640d726bb5aff561f596a99eb59533454
Hunter Parcells <hparcells@icloud.com>
2019-01-05 11:53:18 -0500

    Revert "Kill Chicken"
    This reverts commit 6577189b96888ac46f31638b0931f0347eb5a209.

--- scenes/genocide.jsx
+++ scenes/genocide.jsx
@@ -18,7 +18,7 @@ addScenes({
             { text: "Touch the spider", disabledText: "(dead)", to: "touch_spider", if: () => false },
             { text: "Touch the human", to: "genocide_human" },
             { text: "Touch the unicorn", to: "genocide_unicorn" },
-            { text: "Touch the chicken", disabledText: "(dead)", to: "genocide_chicken", if: () => touch_alive.chicken },
+            { text: "Touch the chicken", to: "genocide_chicken" },
         ],
         contributor: "Hunter and Colyderp"
     },
@@ -38,7 +38,7 @@ addScenes({
             You use the spider corpse to distract the chicken.
         </div>,
         options: [
-            { text: "Kill it", to: "genocide_chicken_kill", action: () => touch_alive.chicken = false },
+            { text: "Kill it", to: "genocide_chicken_kill" },
             { text: "Act like a chicken", to: "genocide_chicken_act" },
         ]
     },
@@ -51,42 +51,6 @@ addScenes({
             name: "Chicken Swarm Ending",
             description: "Die from a swarm of chicken."
         }
-    },
-    genocide_chicken_kill: {
-        prompt: () => <div>
-            <p>You easily killed the chicken while it was distracted.</p>
-            <p className="inventory-update">
-                + Added Chicken Corpse to Inventory.
-            </p>
-            <p>What do you do with it?</p>
-        </div>,
-        options: [
-            { text: "Throw it in the furnace.", to: "" },
-            { text: "Eat it.", to: "" },
-            { text: "Give it to the dogs.", to: "" },
-            { text: "Keep it.", to: "genocide_main" },
-        ],
-        contributor: "Hunter"
-    },
-    genocide_chicken_act: {
-        prompt: () => <div>
-            <p>You start to act like a chicken. It seems very confused. What now?</p>
-        </div>,
-        options: [
-            { text: "(try to) Kill it", to: "genocide_chicken_kill_fail" },
-            { text: "Keep it", to: "genocide_chicken_keep" },
-            { text: "Act like a chicken", to: "genocide_chicken_act", if: () => false },
-            { text: "Distract it", to: "genocide_chicken_distract" },
-        ],
-        contributor: "Hunter"
-    },
-    genocide_chicken_keep: {
-        prompt: () => <div>
-            <p></p>
-        </div>,
-        options: [
-
-        ],
-        contributor: "Hunter"
     }
+    
 });
