32ae99f98defd1193a6675c0f3470750f54c9d9e
Hunter Parcells <hparcells@icloud.com>
2019-01-13 13:09:34 -0500

    Hashbrown Death and Start

--- scenes/breakfast/hashbrowns.jsx
+++ scenes/breakfast/hashbrowns.jsx
@@ -3,5 +3,25 @@ import { } from "web-text-adventure";
 import { addScenes } from "@src/ending";
 
 addScenes({
-
+    make_hashbrowns: {
+        prompt: () => <div>
+            <p>Hashbrowns are pretty neat. Lets make some! Pick your flavor.</p>
+        </div>,
+        options: [
+            { text: "Hashbrowns", to: "hashbrown_death" },
+            { text: "Hashbrownies", to: "" }
+        ],
+        contributor: "Adr"
+    },
+    hashbrown_death: {
+        prompt: () => <div>
+            <p>When the hashbrowns are done making, you choke on them. Nobody is there to save you nor can you save yourself.You eventually pass out and die.</p>
+        </div>,
+        ending: {
+            id: "hashbrowns",
+            name: "Potato Death",
+            description: "How did you even choke on something that easy to eat?"
+        },
+        contributor: "Colyderp"
+    }
 });
