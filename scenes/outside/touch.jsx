45a38c856a338e6ec79c200c655fd43cb563b492
Hunter Parcells <hparcells@icloud.com>
2019-01-29 18:15:44 -0500

    Ending Not Found

--- scenes/outside/touch.jsx
+++ scenes/outside/touch.jsx
@@ -5,13 +5,13 @@ import { addFlag } from "web-text-adventure/src/adventure";
 addScenes({
     touch_main: {
         prompt: () => <div>
-            <p>TODO: yes we know this is empty, (dave: im not sure what goes here or what the plan is)</p>
+            <p>Hey looks like you found a path that doesn't exist. Turns out, we didn't have any ideas for this path, and gave up on it. Get a free ending yay!</p>
         </div>,
-        options: [
-            { text: "", to: "" },
-            { text: "", to: "" },
-            { text: "", to: "" }
-        ],
-        contributor: "Dave"
+        ending: {
+            id: "free-ending",
+            name: "404 Ending Not Found",
+            description: "Its literally been empty for like three weeks."
+        },
+        contributor: "Hunter"
     }
 });
