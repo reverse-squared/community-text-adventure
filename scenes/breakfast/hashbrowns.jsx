144935c658431fd864bef46b8c253d239f2f021a
Hunter Parcells <hparcells@icloud.com>
2019-01-30 10:12:20 -0500

    Some Mario Stuff

--- scenes/breakfast/hashbrowns.jsx
+++ scenes/breakfast/hashbrowns.jsx
@@ -427,6 +427,7 @@ addScenes({
     },
     hash_potatokart_further: {
         prompt: () => <div>
+            <KartHeader />
             <p>You got hit by a <span color={{color: "#3549ff"}}>Blue Shell</span>! You are now in <span style={{color: "#d39a30"}}>3rd place</span>!</p>
         </div>,
         action: () => kartPlace = 3,
@@ -447,10 +448,184 @@ addScenes({
             { text: "Say FRICK again", to: "" },
             { text: "Speak to the bomb dudes", to: "" },
             { text: "Go forward", to: "" },
-            { text: "Try to do a BLJ", to: "" },
+            { text: "Try to do a BLJ", to: "hash_potatokart_blj" },
             { text: "Leave the level", to: "" },
             { text: "Stand there", to: "" }
         ],
         contributor: "Durvenson"
+    },
+    hash_potatokart_blj: {
+        prompt: () => <div>
+            <p>You find a parallel universe.</p>
+        </div>,
+        options: [
+            { text: "Go forward", to: "hash_potatokart_blj_forward" },
+            { text: "BLJ Again", to: "" },
+            { text: "Leave the level", to: "hash_potatokart_blj_leave" },
+            { text: "Stand there", to: "" },
+            { text: "Move forward", to: "" },
+            { text: "Upload your \"discovery\" to Youtube", to: "hash_potatokart_blj_upload" }
+        ],
+        contributor: "Durvenson"
+    },
+    hash_potatokart_blj_upload: {
+        prompt: () => <div>
+            <p>Turns out that you had 2 copyright strikes, and <a href="https://www.youtube.com/channel/UCMm211NGh4Ls5SAMZJF7E8A" target="_blank">pannenkoek2012</a> took down your video due to copyright infringment.</p>
+        </div>,
+        ending: {
+            id: "pannenkoek2012-strike",
+            name: "Copyrighted",
+            description: "Be careful about what you upload..."
+        },
+        contributor: "Durvenson"
+    },
+    hash_potatokart_blj_forward: {
+        prompt: () => <div>
+            <p>You get suddenly a message from TJ Henry Yoshi, which says "A A press is a A press. You can't say it's half" because he thought you were pannenkoek2012.</p>
+        </div>,
+        options: [
+            { text: "Ignore it", to: "" },
+            { text: "I am not pannenkoek2012", to: "" },
+            { text: "That is not true", to: "" },
+            { text: "LIAR! GO KYS", to: "" },
+            { text: "Fight him", to: "" },
+            { text: "Leave the level", to: "hash_potatokart_blj_forward_leave" }
+        ],
+        contributor: "Durvenson"
+    },
+    hash_potatokart_blj_forward_leave: {
+        prompt: () => <div>
+            <p>Your game crashed, which means you died.</p>
+        </div>,
+        ending: {
+            id: "game-crash",
+            name: "Game Crash",
+            description: "Video games can be deadly sometimes."
+        },
+        contributor: "Durvenson"
+    },
+    hash_potatokart_blj_leave: {
+        prompt: () => <div>
+            <p>You are in the castle. Which room do you go to?</p>
+        </div>,
+        options: [
+            { text: "Bob omb battlefield", to: "" },
+            { text: "Cool, cool mountain", to: "" },
+            { text: "Jolly roger bay", to: "" },
+            { text: "Whomp's fortress", to: "" },
+            { text: "Bowser in the dark world", to: "" },
+            { text: "Secret level", to: "" },
+            { text: "Upper floor", to: "" },
+            { text: "Basement", to: "" },
+            { text: "Outside", to: "hash_potatokart_blj_leave_outside" }
+        ],
+        contributor: "Durvenson"
+    },
+    hash_potatokart_blj_leave_outside: {
+        prompt: () => <div>
+            <p>You are now outside of the castle.</p>
+        </div>,
+        options: [
+            { text: "Go back in the castle", to: "" },
+            { text: "Try to get in the moat while the water level is still high", to: "" },
+            { text: "Yolo to get to the roof", to: "hash_potatokart_blj_leave_outside_yolo" }
+        ],
+        contributor: "Durvenson"
+    },
+    hash_potatokart_blj_leave_outside_yolo: {
+        prompt: () => <div>
+            <p>You yoloed to get to the roof. Do you take the 1-ups?</p>
+        </div>,
+        options: [
+            { text: "Yes", to: "hash_potatokart_blj_leave_outside_yolo_yes" },
+            { text: "Maybe", to: "" },
+            { text: "Some of them", to: "hash_potatokart_blj_leave_outside_yolo_some" },
+            { text: "I guess", to: "" },
+            { text: "A little bit of them", to: "" },
+            { text: "No", to: "" },
+            { text: "Nope", to: "" },
+            { text: "Never", to: "" },
+            { text: "WHY", to: "" },
+            { text: "It's time to stop", to: "" },
+            { text: "GET OUT OF MY SWAMP", to: "" },
+            { text: "These are supicious", to: "" }
+        ],
+        contributor: "Durvenson"
+    },
+    hash_potatokart_blj_leave_outside_yolo_yes: {
+        prompt: () => <div>
+            <p>Actually, you find out that they are cocaine after eating one. Will you smoke the rest?</p>
+        </div>,
+        options: [
+            { text: "Yes", to: "" },
+            { text: "Maybe", to: "" },
+            { text: "Some of them", to: "" },
+            { text: "I guess", to: "" },
+            { text: "A little bit of them", to: "" },
+            { text: "No", to: "" },
+            { text: "Nope", to: "" },
+            { text: "Never", to: "" },
+            { text: "WHY", to: "" },
+            { text: "It's time to stop", to: "" },
+            { text: "I DON'T WANT THE FBI TO BE HERE", to: "" },
+            { text: "This is dumb", to: "" }
+        ],
+        contributor: "Durvenson"
+    },
+    hash_potatokart_blj_leave_outside_yolo_some: {
+        prompt: () => <div>
+            <p>You decide to split them in half. You still realize that it is cocaine, but it isn't as bad, and you don't eat it. Instead, you try to escape.</p>
+        </div>,
+        options: [
+            { text: "To the \"hills\"", to: "" },
+            { text: "On a tree", to: "" },
+            { text: "On a \"wall\" of the castle", to: "hash_potatokart_blj_leave_outside_yolo_some_wall" },
+            { text: "In the castle", to: "" },
+            { text: "Underwater", to: "" },
+            { text: "Do nothing", to: "" }
+        ],
+        contributor: "Durvenson"
+    },
+    hash_potatokart_blj_leave_outside_yolo_some_wall: {
+        prompt: () => <div>
+            <p>You fall into an area.</p>
+        </div>,
+        options: [
+            { text: "Try and get out", to: "hash_potatokart_blj_leave_outside_yolo_some_wall_try" },
+            { text: "Go through the door", to: "hash_potatokart_blj_leave_outside_yolo_some_wall_door" }
+        ],
+        contributor: "Durvenson"
+    },
+    hash_potatokart_blj_leave_outside_yolo_some_wall_try: {
+        prompt: () => <div>
+            <p>You are stuck inside.</p>
+        </div>,
+        options: [
+            { text: "BREAK THE WALL", to: "hash_potatokart_blj_leave_outside_yolo_some_wall_try_break" },
+            { text: "Go through the door", to: "hash_potatokart_blj_leave_outside_yolo_some_wall_door" }
+        ],
+        contributor: "Durvenson"
+    },
+    hash_potatokart_blj_leave_outside_yolo_some_wall_try_break: {
+        prompt: () => <div>
+            <p>You injured yourself so bad that you lost an arm.</p>
+        </div>,
+        ending: {
+            id: "break-arm-wall",
+            name: "The Least You Need to be a Amputee",
+            description: "Don't break walls!"
+        },
+        contributor: "Durvenson"
+    },
+    hash_potatokart_blj_leave_outside_yolo_some_wall_door: {
+        prompt: () => <div>
+            <p>You get into another area.</p>
+        </div>,
+        options: [
+            // TODO: VVV Goes to castle.
+            { text: "Go into a wall", to: "" },
+            { text: "Go through the door", to: "hash_potatokart_blj_leave_outside_yolo_some_wall" },
+            { text: "Wall jump out", to: "hash_potatokart_blj_leave_outside_yolo_some_wall_try_break" }
+        ]
     }
 });
