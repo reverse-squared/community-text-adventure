7146d31472610b7b2ec474e8b2b00d01d5d37c9f
Dave <imdaveead@gmail.com>
2019-02-02 13:27:42 -0500

    3 wires

--- scenes/breakfast/ktane.jsx
+++ scenes/breakfast/ktane.jsx
@@ -6,6 +6,30 @@ function randomOf(list) {
     return list[Math.floor((Math.random() * list.length))];
 }
 
+const MissingItem = Symbol("MissingItem");
+function randomListOf(list, items = 1, requiredValues = []) {
+    let requiredValuesPositions = requiredValues.map(() => -1);
+    let values = Array(items).fill(MissingItem);
+    requiredValues.forEach((item, i) => {
+        let position;
+        do {
+            position = Math.floor(Math.random() * items);
+        } while (requiredValuesPositions.includes(position));
+
+        requiredValuesPositions[i] = position;
+    });
+    requiredValuesPositions.forEach((position, i) => {
+        values[position] = requiredValues[i];
+    });
+    return values.map(item => {
+        if(item === MissingItem) return randomOf(list);
+        return item;
+    });
+}
+
+// Bomb Manual
+// http://www.bombmanual.com/manual/1/html/index.html
+
 addFlag("wires", false);
 addFlag("buttom", false);
 addFlag("symbols", false);
@@ -23,10 +47,42 @@ addFlag("morseOptions", [
 ]);
 
 addFlag("wires", null);
+const allWireColors = ["yellow", "blue", "white", "black"];
 function generateWires() {
+    const correct = true;
     const wireCount = randomOf([3, 4, 5, 6]);
     if (wireCount === 3) {
-        //
+        // 3 Wires
+        switch (randomOf([1,2,3,4])) {
+        case 1: { // If there are no red wires, cut the second wire.
+            const colorsNotRed = allWireColors.filter(x => x !== "red");
+            wires = [
+                { color: randomOf(colorsNotRed) },
+                { color: randomOf(colorsNotRed), correct },
+                { color: randomOf(colorsNotRed) },
+            ];
+            break;
+        }
+        case 2: { // Otherwise, if the last wire is white, cut the last wire.
+            wires = [
+                ...randomListOf(allWireColors, 2, ["red"]).map(color => ({ color })),
+                { color: "white", correct }
+            ];
+            break;
+        }
+        case 3: {// Otherwise, if there is more than one blue wire, cut the last blue wire.
+            wires = randomListOf([], 3, ["blue", "blue", "red"]).map((color, i, array) => ({ color, correct: array[0] === "red" ? i == 2 : color === "blue" && i !== 0 }));
+            break;
+        }
+        case 4: {// Otherwise, cut the last wire.
+            const colorsNotBlue = allWireColors.filter(x => x !== "blue");
+            wires = [
+                ...randomListOf([], 2, ["blue", "red"]).map(color => ({ color })),
+                { color: randomOf(colorsNotBlue), correct },
+            ];
+            break;
+        }
+        }
     } else if (wireCount === 4) {
         //
     } else if (wireCount === 5) {
@@ -34,6 +90,7 @@ function generateWires() {
     } else if (wireCount === 6) {
         //
     }
+    return wires;
 }
 
 addScenes({
@@ -54,7 +111,4 @@ addScenes({
         contributor: "Hunter"
     },
     // #endregion
-    //#region
-    
-    //#endregion
 });
